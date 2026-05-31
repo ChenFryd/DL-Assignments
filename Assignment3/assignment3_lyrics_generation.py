cf05182edde2a2f064ac6ed6904093080f2b48e2
Amit Ner Gaon <amitner@post.bgu.ac.il>
2026-05-31 18:08:04 +0300

    make files

---
+++ Assignment3/assignment3_lyrics_generation.py
@@ -0,0 +1,37 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Assignment 3 - Lyrics Generation
+#
+# * Chen Frydman - 208009845
+# * Amit Ner-Gaon - 211649801
+#
+# Ben-Gurion University of the Negev - Faculty of Computer & Information Science
+#
+#
+# This .ipynb (for human readability) or .py (for AI readability) file is sync with the file with the same name and other extension (using `jupytext`).
+#
+# See `Assignment3/conda_requirement.yaml`
+
+# %%
+# test
+import numpy as np
+import torch
+
+# %%
+# test
+print("Notebook is working")
